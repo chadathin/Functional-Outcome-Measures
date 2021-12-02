16833cfc69f69b5e248e137d2b94487d29c241b4
Chad <chad.boehm@gmail.com>
2021-12-02 14:52:42 -0600

    koos test
    Can pull values from form, sum and send everything to "results" page

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_mysqldb import MySQL
-#import dbconfig
+# import db_config
 
 app = Flask(__name__,
             static_folder='assets',
             template_folder='templates')
+app.secret_key = "tis_but_a_key"
 
 @app.route("/")
 def home():
     return render_template("base.html")
 
-@app.route("/score", methods=["POST", "GET"])
-def score():
+@app.route("/result", methods=["POST", "GET"])
+def result():
     if request.method == "POST":
+        # Get all form radio values
         submission = request.form
+
+        # Get the incoming assessment name
+        assessment = session['assessment']
+
+        # Sanitizes selected form values
         results = []
+
         for question in submission:
             results.append(int(submission[question]))
-        print(results)
-        return render_template("koos.html")
+
+        result = sum(results)
+        return render_template("results_page.html", result=result, assessment=assessment)
     else:
-        return render_template("koos.html")
+        return "No assessment performed!"
+
+@app.route("/koos", methods=["POST", "GET"])
+def koos():
+    session['assessment'] = "koos"
+    return render_template("koos.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
