1e77fa752017882f859a383828b821d317a43ccc
Chad <chad.boehm@gmail.com>
2021-12-02 21:44:48 -0600

    Update assessments.sql
    Added optional info (weight, age, sex)

--- SQL/assessments.sql
+++ SQL/assessments.sql
@@ -1,9 +1,16 @@
 DROP TABLE IF EXISTS assessments;
 
 CREATE TABLE `assessments` (
-  `id` int(32) AUTO_INCREMENT NOT NULL,
-  `unique_ident`varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `score` int(32) NOT NULL,
-  PRIMARY KEY(`id`)
-);
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `unique_ident` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
+  `name` VARCHAR(255) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
+  `score` INT(11) NOT NULL DEFAULT '0',
+  `date_performed` DATE NOT NULL DEFAULT curdate(),
+  `age` INT(11) NULL DEFAULT NULL,
+  `sex` CHAR(1) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+  `weight` INT(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
