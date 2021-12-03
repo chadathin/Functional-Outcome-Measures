c0ff1c6cadb66ee59b50664dbbdf7ff30044e0ec
Chad <chad.boehm@gmail.com>
2021-12-02 22:47:03 -0600

    connected mariaDB
    I am able to send data to database!

--- app.py
+++ app.py
@@ -1,19 +1,58 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-
+import sys
 from flask_mysqldb import MySQL
 
 # ========== My Modules ====================
 
 from funcs import randString
 from koos import koos_lookup
+import mariadb
 # import db_config
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+
+# app.config['MYSQL_HOST'] = '127.0.0.1:3307'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'password'
+# app.config['MYSQL_DB'] = 'foms'
+#
+# mysql = MySQL(app)
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="root",
+        password="Nicole0406!",
+        host="127.0.0.1",
+        port=3307,
+        database="foms"
+    )
+    print("Database connected!")
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+conn.autocommit = False
+
+# Get Cursor
+cur = conn.cursor()
 
 assessmentID = randString()
 
 app.secret_key = assessmentID
 
+def insertSomething(cursor):
+    # cur = conn.cursor()
+
+    cursor.execute("INSERT INTO assessments (unique_ident, name, score) VALUES (?, ?, ?);",
+                ("ABCDF", "QuickDash", 55))
+    print("sent!")
+    conn.commit()
+                                                        # ============= NOTE TO SELF =============
+    conn.close()                                        # RELOADER IS CAUSING TWO EXECUTES TO BE SENT
+    return 0                                            # CAN STOP BY TURNING OFF DEBUG, OR MOVING THIS FUNCTION TO NOT BE GLOBAL
+
+insertSomething(cur)
 
 avail_assessments = {
     "KOOS JR": '/koos',
--- funcs.py
+++ funcs.py
@@ -14,3 +14,14 @@ def randString():
     for _ in range(8):
         out+=choice(select)
     return out
+
+def insertSomething(sql_obj):
+    cur = sql_obj.connection.cursor()
+
+    cur.execute("INSERT INTO assessments (unique_ident, name, score) VALUES (%s, %s, %s, %s)",
+                ("ABCDE", "QuickDash", 75))
+
+    sql_obj.connection.commit()
+
+    cur.close()
+    return 0
